e7ea78795a64191b67c908bc3870560f585b9888
Helen <helen@Helens-MacBook-Pro.local>
2018-07-17 11:32:14 -0500

    retrieving data from local storage.

---
+++ database.js
@@ -0,0 +1,9 @@
+const loadDatabase = function (localStorageKey) {
+    // Get the string version of the database
+    const databaseString = localStorage.getItem(localStorageKey)
+
+    // Use JSON.parse() to convert the string back into an object
+    return JSON.parse(databaseString)
+}
+
+console.log(loadDatabase("HomeInventory"));
